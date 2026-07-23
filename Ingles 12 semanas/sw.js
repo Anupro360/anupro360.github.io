95a11ef5e5bb454a3e80e4ed5e1d04820734828c
Anupro360 <contreras.munoz@gmail.com>
2026-07-23 15:15:13 -0400

    Add files via upload

---
+++ Ingles 12 semanas/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ingles-12sem-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
